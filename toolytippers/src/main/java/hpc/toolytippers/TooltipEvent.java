4e4c4c9d87eab41af95541ef10d5ccd8a01720e3
databox <headprogrammingczar@gmail.com>
2016-03-17 16:21:21 -0400

    more tooltip stuff

--- toolytippers/src/main/java/hpc/toolytippers/TooltipEvent.java
+++ toolytippers/src/main/java/hpc/toolytippers/TooltipEvent.java
@@ -1,8 +1,12 @@
 package hpc.toolytippers;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
@@ -11,12 +15,32 @@ public class TooltipEvent {
 
 	@SubscribeEvent
 	public void onTooltip(ItemTooltipEvent event) {
+		EnumChatFormatting color = EnumChatFormatting.BLUE;
 		if (event.itemStack != null) {
 			ItemStack itemStack = event.itemStack;
 			int maxDurability = itemStack.getMaxDamage();
 			if (maxDurability > 1 && !itemStack.getHasSubtypes()) {
 				int currentDurability = maxDurability - itemStack.getItemDamage();
-				event.toolTip.add("Durability: " + currentDurability + "/" + maxDurability);
+				event.toolTip.add(color + "Durability: " + currentDurability + "/" + maxDurability);
+			}
+			if (itemStack.getItem() instanceof ItemArmor) {
+				ItemArmor armor = ((ItemArmor) itemStack.getItem());
+				int protection = armor.damageReduceAmount;
+				if (protection > 0) {
+					event.toolTip.add(color + "+" + protection + " Defense");
+				}
+				if (itemStack.getItem().getItemEnchantability() > 0) {
+					int enchantability = itemStack.getItem().getItemEnchantability();
+					int[] enchantabilities = {9, 10, 12, 15, 25, 26, 30};
+					String[] enchantabilityNames = {"Iron", "Diamond", "Chain", "Leather", "Gold", "Better than Gold", "Better than wolves(tm)"};
+					String betterThan = "Worse than Iron";
+					for (int i = 0; i < enchantabilities.length && enchantability >= enchantabilities[i]; i++) {
+						betterThan = enchantabilityNames[i];
+					}
+					if (itemStack.getEnchantmentTagList() == null) {
+						event.toolTip.add(color + "Enchantability: " + betterThan);
+					}
+				}
 			}
 			int miningLevel = 0;
 			String[] tiers = {"Wood", "Stone", "Iron", "Diamond", "Alumite", "Manyullyn"};
@@ -29,9 +53,9 @@ public class TooltipEvent {
 				} else {
 					toolTier = (miningLevel - tiers.length) + " tiers above " + tiers[tiers.length - 1];
 				}
-				event.toolTip.add("Tier: " + toolTier);
-				float[] efficiencies = {2.0f, 4.0f, 6.0f, 8.0f, 12.0f, 14.0f};
-				String[] efficiencyNames = {"Wood", "Stone", "Iron", "Diamond", "Gold", "Faster than Gold"};
+				event.toolTip.add(color + "Tier: " + toolTier);
+				float[] efficiencies = {2.0f, 4.0f, 6.0f, 8.0f, 12.0f, 14.0f, 20.0f};
+				String[] efficiencyNames = {"Wood", "Stone", "Iron", "Diamond", "Gold", "Faster than Gold", "Faster than water!"};
 				float miningSpeed = tool.func_150913_i().getEfficiencyOnProperMaterial();
 				NBTTagList enchantments = itemStack.getEnchantmentTagList();
 				int efficiencyLevel = 0;
@@ -49,7 +73,26 @@ public class TooltipEvent {
 				for (int i = 0; i < efficiencies.length && miningSpeed >= efficiencies[i]; i++) {
 					fasterThan = efficiencyNames[i];
 				}
-				event.toolTip.add("Mining Speed: " + fasterThan);
+				event.toolTip.add(color + "Mining Speed: " + fasterThan);
+				if (itemStack.getItem().getItemEnchantability() > 0) {
+					int enchantability = itemStack.getItem().getItemEnchantability();
+					int[] enchantabilities = {5, 10, 14, 15, 22, 23};
+					String[] enchantabilityNames = {"Stone", "Diamond", "Iron", "Wood", "Gold", "Better than Gold", "Better than wolves(tm)"};
+					String betterThan = "Worse than Stone";
+					for (int i = 0; i < enchantabilities.length && enchantability >= enchantabilities[i]; i++) {
+						betterThan = enchantabilityNames[i];
+					}
+					if (itemStack.getEnchantmentTagList() == null) {
+						event.toolTip.add(color + "Enchantability: " + betterThan);
+					}
+				}
+			}
+			if (itemStack.getItem() instanceof ItemBlock) {
+				Block block = ((ItemBlock) itemStack.getItem()).field_150939_a;
+				float power = block.getEnchantPowerBonus(event.entityPlayer.worldObj, 0, 0, 0);
+				if (power > 0) {
+					event.toolTip.add(color + "Enchanting: " + power + " Bookshelves");
+				}
 			}
 		}
 	}
